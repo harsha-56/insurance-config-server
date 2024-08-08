f12dd60d79d06bc3ad3cd7d64212eac3f0c60acc
herukala <harshavardhinierukala67@gmail.com>
2024-08-08 13:46:55 +0530

    changes in services

---
+++ insurance-client/src/test/java/com/example/insurance_client/InsuranceClientServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.insurance_client;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InsuranceClientServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
---
+++ insurance-provider/src/test/java/com/example/insurance_provider/InsuranceProviderApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.insurance_provider;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InsuranceProviderApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
