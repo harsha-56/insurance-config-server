4cc92a5cd2c77fbcb2bbd9b5b3fc8b3add21fb36
herukala <harshavardhinierukala67@gmail.com>
2024-08-08 13:58:43 +0530

    config server

---
+++ src/test/java/com/example/insurance_server/InsuranceConfigServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.insurance_server;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InsuranceConfigServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
